6adb0c97335e5f1ce772f2a4d6e8a58aa321f5c3
Shayan Tehrani <shayanthrn@gmail.com>
2020-06-22 06:22:13 +0000

    update dev product

---
+++ test.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const router = require('./Router/router.js');  
+const bodyParser = require('body-parser');   //for parsing and getting data from http post request
+var cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+
+app.use(express.static('public'));    // files on the public path are downloadable
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(fileUpload({useTempFiles : true}))
+
+app.set('views','./htmls');
+app.engine('html', require('ejs').renderFile);
+app.get('/',function(req,res){
+    res.write("<html><body><p>test</p></body></html>");
+    res.end();
+})
+
+const port = process.env.port || 80;   // server port for listen
+app.listen(port, () => console.log(`Run on port ${port}`));   //listen
