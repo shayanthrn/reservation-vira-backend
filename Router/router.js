bf1bf029c51ab452e094d19a933d4de2ce0bf941
shayan tehrani <shayanthrn@gmail.com>
2020-06-24 13:21:05 +0430

    fix upload photo

--- Router/router.js
+++ Router/router.js
@@ -229,6 +229,7 @@ router.post("/addDoctor",function(req,res){
           dbo.collection('Doctors').insertOne(new Doctor(req.body.username,req.body.pass,req.body.name,req.body.categories,req.body.medicalnumber,req.body.codemeli,req.body.workphone,req.body.phonenumber,req.body.address,req.body.city,"/docphotos/"+req.body.name+".png",req.body.background,req.body.description,req.body.membershiptypes,req.body.appknowledge),function(err,res2){
             if(req.files!=null){
               mv(req.files.image.tempFilePath,"public/docphotos/"+req.body.name+".png",function(err){
+                console.log("public/docphotos/"+req.body.name+".png")
               })
             }
             res.redirect('/'); //fixxxxxxxxxxxxxxxxxxxxxxxx
