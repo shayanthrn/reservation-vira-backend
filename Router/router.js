1988155db2e9f1c4577ce39694449e5abb3f56ee
shayan tehrani <shayanthrn@gmail.com>
2020-06-28 10:28:38 +0430

    debug

--- Router/router.js
+++ Router/router.js
@@ -424,7 +424,7 @@ router.post("/addDoctor",function(req,res){
     MongoClient.connect(dburl,function(err,db){
       var dbo=db.db("mydb");
       var cats=[];
-      console.log(typeof stringValue=="string")
+      console.log(typeof stringValue==string)
       if(typeof stringValue=="string"){
         cats.push(req.body.categories);
       }
