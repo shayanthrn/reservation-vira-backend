9714043e01d766e8e88872035435400db1be6e9c
shayan tehrani <shayanthrn@gmail.com>
2020-06-30 11:24:26 +0430

    add some categories

--- database.js
+++ database.js
@@ -4,19 +4,14 @@ var dburl="mongodb://localhost:27017/";
 
 MongoClient.connect(dburl,function(err,db){
     var dbo=db.db("mydb");
-    dbo.collection("Categories").updateOne({name:"ارتپدی"},{$set:{name:"ارتوپدی"}});
-    dbo.collection("Categories").deleteOne({name:"اورولوژی"});
-    dbo.collection("Categories").insertOne({name:"بیماری های عفونی و تب دار",image:"/catphotos/ofoni.png"}); 
-    dbo.collection("Categories").insertOne({name:"جراح کلیه و مجاری ادراری",image:"/catphotos/kolie.png"});
-    dbo.collection("Categories").insertOne({name:"قلب و عروق",image:"/catphotos/qalb.png"});
-    dbo.collection("Categories").insertOne({name:"چشم",image:"/catphotos/cheshm.png"});
-    dbo.collection("Categories").insertOne({name:"اطفال",image:"/catphotos/atfal.png"});
-    dbo.collection("Categories").insertOne({name:"روانپرشک(اعصاب و روان)",image:"/catphotos/ravan.png"});
-    dbo.collection("Categories").insertOne({name:"گوش حلق بینی",image:"/catphotos/goshhalq.png"});
-    dbo.collection("Categories").insertOne({name:"تغذیه",image:"/catphotos/taqzie.png"});
-    dbo.collection("Categories").insertOne({name:"جراحی مغز و اعصاب",image:"/catphotos/maqz.png"});
-    dbo.collection("Categories").insertOne({name:"جراحی و متخصص زنان",image:"/catphotos/jarahizanan.png"});
-    dbo.collection("Categories").insertOne({name:"دندانپزشک",image:"/catphotos/dandan.png"});
-    dbo.collection("Categories").insertOne({name:"غدد و متابولیسم",image:"/catphotos/qodad.png"});
-    dbo.collection("Categories").insertOne({name:"جراحی عمومی",image:"jarahi.png"});
+    dbo.collection("Categories").insertOne({name:"مامایی",image:"/catphotos/مامایی.png"});
+    dbo.collection("Categories").insertOne({name:"پزشک عمومی",image:"/catphotos/پزشک-عمومی.png"});
+    dbo.collection("Categories").insertOne({name:"شنوایی سنجی",image:"/catphotos/شنوایی-سنجی.png"});
+    dbo.collection("Categories").insertOne({name:"بینایی سنجی",image:"/catphotos/بینایی-سنجی.png"});
+    dbo.collection("Categories").insertOne({name:"جراحی پلاستیک و زیبایی",image:"/catphotos/جراحی-پلاستیک-و-زیبایی.png"});
+    dbo.collection("Categories").insertOne({name:"خون و سرطان",image:"/catphotos/خون-و-سرطان.png"});
+    dbo.collection("Categories").insertOne({name:"بیهوشی و مراقبت ویژه",image:"/catphotos/بیهوشی-و-مراقبت-ویژه.png"});
+    dbo.collection("Categories").insertOne({name:"ریه",image:"/catphotos/ریه.png"});
+    dbo.collection("Categories").insertOne({name:"گوارش و کبد",image:"/catphotos/گوارش-و-کبد.png"});
+    dbo.collection("Categories").insertOne({name:"مغز و اعصاب",image:"/catphotos/مغز-و-اعصاب.png"});
 })
