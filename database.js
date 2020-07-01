e1b1289e8399ae91820fc6eb24d940d53dc95303
shayan tehrani <shayanthrn@gmail.com>
2020-07-01 15:54:38 +0430

    db config

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ var dburl="mongodb://localhost:27017/";
 
 MongoClient.connect(dburl,function(err,db){
     var dbo=db.db("mydb");
-    dbo.collection("Categories").updateOne({name:"روانپرشک(اعصاب و روان)"},{$set:{image:"/catphotos/روانپزشک(اعصاب-و-روان).png"}},function(err,result){
+    dbo.collection("Categories").updateOne({name:"روانپرشک(اعصاب و روان)"},{$set:{name:"روانپزشک(اعصاب و روان)"}},function(err,result){
         console.log(result)
     })
 })
