daba13170a26aade5b2cd851884d51ae40d0373e
shayan tehrani <shayanthrn@gmail.com>
2020-07-23 13:21:26 +0430

    add hc-res-info

--- Router/router.js
+++ Router/router.js
@@ -2107,7 +2107,7 @@ router.get("/reservation/:type/:HCname/:category",function(req,res){
         days.push(currentday);
         freetimes.push(getDoctimeslots(catobj,new myDate(currentday.toArray()[2],currentday.toArray()[1],currentday.toArray()[0])));
       }
-      res.render("reserve.ejs",{doctor:HC,days:createDayboxobj(days),freetimes:freetimes});
+      res.render("reserve.ejs",{doctor:result,days:createDayboxobj(days),freetimes:freetimes});
       res.end();
     }
   })
