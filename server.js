492a233c1fabdbaa788d795f80a379c5ffa4b063
shayan tehrani <shayanthrn@gmail.com>
2020-09-05 12:32:03 +0430

    check https server

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.set('views','./htmls');
 app.engine('html', require('ejs').renderFile);
 app.use('/',router);
 
-var privateKey  = fs.readFileSync('server.key', 'utf8');
-var certificate = fs.readFileSync('server.cert', 'utf8');
+var privateKey  = fs.readFileSync('ssl/keys/server.key', 'utf8');
+var certificate = fs.readFileSync('ssl/certs/server.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
