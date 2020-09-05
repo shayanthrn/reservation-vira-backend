5bd68142a1175f50b706b8ffec70e069e03cf805
shayan tehrani <shayanthrn@gmail.com>
2020-09-05 15:24:28 +0430

    add md5

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.set('views','./htmls');
 app.engine('html', require('ejs').renderFile);
 app.use('/',router);
 
-var privateKey  = fs.readFileSync('letsencrypt/live/reservation.drtajviz.com/privkey.pem', 'utf8');
-var certificate = fs.readFileSync('letsencrypt/live/reservation.drtajviz.com/fullchain.pem', 'utf8');
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/reservation.drtajviz.com/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/reservation.drtajviz.com/fullchain.pem', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
 var httpServer = http.createServer(app);
