292281f804c363826a663e1c2d54e93d888248e0
shayan tehrani <shayanthrn@gmail.com>
2020-09-05 11:58:52 +0430

    check https server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');   //for parsing and getting data from
 var cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
+const https = require('https');
+var fs = require('fs');
+
 
 
 app.use(express.static('public'));    // files on the public path are downloadable
@@ -18,6 +21,13 @@ app.set('views','./htmls');
 app.engine('html', require('ejs').renderFile);
 app.use('/',router);
 
+const httpsServer = https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}, app);
 
-const port = process.env.port || 80;   // server port for listen
-app.listen(port, () => console.log(`Run on port ${port}`));   //listen
+
+httpsServer.listen(443, () => console.log(`HTTPS server listening: https://localhost`));
+
+// const port = process.env.port || 80;   // server port for listen
+// app.listen(port, () => console.log(`Run on port ${port}`));   //listen
